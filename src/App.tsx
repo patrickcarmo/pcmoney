679a26b63a3cdb94029764cc13a9747ceeb8ee24
Patrick Carmo <patrick.a.carmo@gmail.com>
2021-04-14 20:38:16 +0100

    styled components and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import styled from 'styled-components';
+
+import { GlobalStyle } from './styles/global'
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>pcmomey</h1>
+      <Title>pcmomey</Title>
+      <GlobalStyle />
     </div>
   );
 }
