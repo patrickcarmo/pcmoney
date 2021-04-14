39fe1f32994fcaa0343ef631148bb29bfce96a7a
Patrick Carmo <patrick.a.carmo@gmail.com>
2021-04-14 21:26:38 +0100

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Header } from './components/Header';
 
 import { GlobalStyle } from './styles/global'
 
@@ -9,9 +10,9 @@ const Title = styled.h1`
 
 export function App() {
   return (
-    <div className="App">
-      <Title>pcmomey</Title>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
